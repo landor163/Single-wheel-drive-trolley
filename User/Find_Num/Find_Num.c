d52ca9a48d21165e1dfe81583f21c425dd1970aa
岛主_Landor <landor@aliyun.com>
2021-07-06 20:48:52 +0800

    添加 新的红外对管程序

--- User/Find_Num/Find_Num.c
+++ User/Find_Num/Find_Num.c
@@ -1,26 +1,47 @@
 /*
 @接线说明：
-		LED_1_out 接 B4 ;
-		LED_2_out 接 B8 ;
-		LED_3_out 接 B9 ;
-		LED_4_out 接 C8 ;
-		LED_5_out 接 C9 ;
+		LED_1_out 接 B4  ;
+		LED_2_out 接 B8  ;
+		LED_3_out 接 B9  ;
+		LED_4_out 接 C8  ;
+		LED_5_out 接 C9  ;
+		LED_6_out 接 C7  ;
+		LED_7_out 接 C10 ;
 */
 
 
 #include "Find_Num.h"
 
 
-void Find_Nun_Init()
+void Find_Nun_Init_B()
 {
 	GPIO_InitTypeDef GPIO_InitStruct;
 
-	RCC_APB2PeriphClockCmd(LED_1_GPIO_CLK|LED_2_GPIO_CLK|LED_3_GPIO_CLK|LED_4_GPIO_CLK|LED_5_GPIO_CLK,ENABLE);
+	RCC_APB2PeriphClockCmd(LED_1_GPIO_CLK|LED_2_GPIO_CLK|LED_3_GPIO_CLK,ENABLE);
 
-	GPIO_InitStruct.GPIO_Pin=LED_1_GPIO_PIN|LED_2_GPIO_PIN|LED_3_GPIO_PIN|LED_4_GPIO_PIN|LED_5_GPIO_PIN;
+	GPIO_InitStruct.GPIO_Pin=LED_1_GPIO_PIN|LED_2_GPIO_PIN|LED_3_GPIO_PIN;
 	GPIO_InitStruct.GPIO_Mode=GPIO_Mode_IPD;
 	GPIO_InitStruct.GPIO_Speed=GPIO_Speed_50MHz;
 	
-	GPIO_Init(LED_2_GPIO_PORT,&GPIO_InitStruct);
+	GPIO_Init(LED_1_GPIO_PORT,&GPIO_InitStruct);
+}
+
+void Find_Nun_Init_C()
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	RCC_APB2PeriphClockCmd(LED_4_GPIO_CLK|LED_5_GPIO_CLK|LED_6_GPIO_CLK|LED_7_GPIO_CLK,ENABLE);
+
+	GPIO_InitStruct.GPIO_Pin=LED_4_GPIO_PIN|LED_5_GPIO_PIN|LED_6_GPIO_PIN|LED_7_GPIO_PIN;
+	GPIO_InitStruct.GPIO_Mode=GPIO_Mode_IPD;
+	GPIO_InitStruct.GPIO_Speed=GPIO_Speed_50MHz;
+	
+	GPIO_Init(LED_4_GPIO_PORT,&GPIO_InitStruct);
+}
+
+void Find_Nun_Init()
+{
+	Find_Nun_Init_B();
+	Find_Nun_Init_C();
 }
 
--- User/Find_Num/Find_Num.h
+++ User/Find_Num/Find_Num.h
@@ -25,14 +25,25 @@
 #define LED_5_GPIO_CLK    RCC_APB2Periph_GPIOB
 #define LED_5_GPIO_PIN    GPIO_Pin_9
 
+#define LED_6_GPIO_PORT   GPIOC
+#define LED_6_GPIO_CLK    RCC_APB2Periph_GPIOB
+#define LED_6_GPIO_PIN    GPIO_Pin_7
+
+#define LED_7_GPIO_PORT   GPIOC
+#define LED_7_GPIO_CLK    RCC_APB2Periph_GPIOB
+#define LED_7_GPIO_PIN    GPIO_Pin_10
+
 #define LED_1_out  GPIO_ReadInputDataBit( LED_1_GPIO_PORT, LED_1_GPIO_PIN);
 #define LED_2_out  GPIO_ReadInputDataBit( LED_2_GPIO_PORT, LED_2_GPIO_PIN);
 #define LED_3_out  GPIO_ReadInputDataBit( LED_3_GPIO_PORT, LED_3_GPIO_PIN);
 #define LED_4_out  GPIO_ReadInputDataBit( LED_4_GPIO_PORT, LED_4_GPIO_PIN);
 #define LED_5_out  GPIO_ReadInputDataBit( LED_5_GPIO_PORT, LED_5_GPIO_PIN);
+#define LED_6_out  GPIO_ReadInputDataBit( LED_6_GPIO_PORT, LED_6_GPIO_PIN);
+#define LED_7_out  GPIO_ReadInputDataBit( LED_7_GPIO_PORT, LED_7_GPIO_PIN);
 
 
+void Find_Nun_Init_B(void);
+void Find_Nun_Init_C(void);
 void Find_Nun_Init(void);
-
 #endif /*Find_Num_H*/
 
